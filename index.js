5b0e970736fcc523b5d904fa8fe578e6b230cca9
E.Azer Koçulu <azer@kodfabrik.com>
2015-11-01 15:32:38 -0800

    impl targetFolder

--- index.js
+++ index.js
@@ -14,18 +14,25 @@ var Starter = struct({
   rename: rename,
   render: render,
   variables: variables,
+  targetFolder: targetFolder,
   serially: serially
 });
 
 module.exports = Starter;
 
 function copy (starter, folder, callback) {
-  debug('Copying from starter "%s" to project "%s"', starter.folder, starter.project.folder);
-  starter.project.exec('cp -r {0}/. .', folder, callback);
+  debug('Copying from starter "%s" to project "%s"', starter.folder, starter.targetFolder());
+  starter.project.exec('cp -r {0}/. {1}/.', folder, starter.targetFolder(), callback);
 }
 
-function format (starter, text) {
-  return formatText(text, formatVars(starter.variables()));
+function format (starter, text, context) {
+  var vars = starter.variables();
+
+  if (context) {
+    mix(vars, [context]);
+  }
+
+  return formatText(text, formatVars(vars));
 }
 
 function rename (starter, map, callback) {
@@ -33,8 +40,8 @@ function rename (starter, map, callback) {
   loop(targets.length, each, callback);
 
   function each (done, index) {
-    var from = path.join(starter.project.folder, targets[index]);
-    var to = path.join(starter.project.folder, starter.format(map[targets[index]]));
+    var from = path.join(starter.targetFolder(), targets[index]);
+    var to = path.join(starter.targetFolder(), starter.format(map[targets[index]]));
 
     debug('Renaming %s to %s', from, to);
 
@@ -46,7 +53,7 @@ function render (starter, files, callback) {
   loop(files.length, each, callback);
 
   function each (done, index) {
-    var filename = path.join(starter.project.folder, files[index]);
+    var filename = path.join(starter.targetFolder(), files[index]);
     debug('Rendering %s', filename);
 
     fs.readFile(filename, function (error, content) {
@@ -61,11 +68,16 @@ function prefix (starter, str, obj) {
   return starter.project.prefix('kik:' + str, obj);
 }
 
+function targetFolder (starter) {
+  if (!starter.subfolder) return starter.project.folder;
+  return path.join(starter.project.folder, starter.subfolder);
+}
+
 function variables (starter) {
   var p = starter.project.variables();
   var s = starter.prefix(starter.name + ':', mix({}, [starter.context, {
     name: starter.name,
-    folder: starter.folder
+    folder: starter.targetFolder()
   }]));
 
   return mix(p, [s]);
