2ead0e1ff5894593dd5ef346742a95b2c3c555cc
E.Azer Koçulu <azer@kodfabrik.com>
2016-01-18 19:26:52 -0800

    use es6 classes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var debug = require("local-debug")('starter');
 var struct = require("new-struct");
 var loop = require("serial-loop");
@@ -7,80 +9,77 @@ var fs = require("fs");
 var mix = require("mix-objects");
 var serially = require("serially");
 
-var Starter = struct({
-  copy: copy,
-  format: format,
-  prefix: prefix,
-  rename: rename,
-  render: render,
-  variables: variables,
-  targetFolder: targetFolder,
-  serially: serially
-});
-
-module.exports = Starter;
-
-function copy (starter, folder, callback) {
-  debug('Copying from starter "%s" to project "%s"', starter.folder, starter.targetFolder());
-  starter.project.exec('cp -r {0}/. {1}/.', folder, starter.targetFolder(), callback);
-}
-
-function format (starter, text, context) {
-  var vars = starter.variables();
-
-  if (context) {
-    mix(vars, [context]);
+class Starter {
+  constructor (name, project, folder, form) {
+    this.name = name;
+    this.project = project;
+    this.folder = folder;
+    this.form = form;
   }
 
-  return formatText(text, formatVars(vars));
-}
-
-function rename (starter, map, callback) {
-  var targets = Object.keys(map);
-  loop(targets.length, each, callback);
-
-  function each (done, index) {
-    var from = path.join(starter.targetFolder(), targets[index]);
-    var to = path.join(starter.targetFolder(), starter.format(map[targets[index]]));
-
-    debug('Renaming %s to %s', from, to);
-
-    fs.rename(from, to, done);
+  copy (folder, callback) {
+    debug('Copying from starter "%s" to project "%s"', this.folder, this.targetFolder());
+    this.project.exec('cp -r {0}/. {1}/.', folder, this.targetFolder(), callback);
   }
-}
 
-function render (starter, files, callback) {
-  loop(files.length, each, callback);
+  format (text, context) {
+    var vars = this.variables();
 
-  function each (done, index) {
-    var filename = path.join(starter.targetFolder(), files[index]);
-    debug('Rendering %s', filename);
+    if (context) {
+      mix(vars, [context]);
+    }
 
-    fs.readFile(filename, function (error, content) {
-      if (error) return done(error);
-
-      fs.writeFile(filename, starter.format(content), done);
-    });
+    return formatText(text, formatVars(vars));
   }
-}
 
-function prefix (starter, str, obj) {
-  return starter.project.prefix('kik:' + str, obj);
-}
+  rename (map, callback) {
+    var targets = Object.keys(map);
+    loop(targets.length, each, callback);
 
-function targetFolder (starter) {
-  if (!starter.subfolder) return starter.project.folder;
-  return path.join(starter.project.folder, starter.subfolder);
-}
+    function each (done, index) {
+      var from = path.join(this.targetFolder(), targets[index]);
+      var to = path.join(this.targetFolder(), this.format(map[targets[index]]));
 
-function variables (starter) {
-  var p = starter.project.variables();
-  var s = starter.prefix(starter.name + ':', mix({}, [starter.context, {
-    name: starter.name,
-    folder: starter.targetFolder()
-  }]));
+      debug('Renaming %s to %s', from, to);
 
-  return mix(p, [s]);
+      fs.rename(from, to, done);
+    }
+  }
+
+  render (files, callback) {
+    var self = this;
+    loop(files.length, each, callback);
+
+    function each (done, index) {
+      var filename = path.join(self.targetFolder(), files[index]);
+      debug('Rendering %s', filename);
+
+      fs.readFile(filename, function (error, content) {
+        if (error) return done(error);
+
+        fs.writeFile(filename, self.format(content), done);
+      });
+    }
+  }
+
+  prefix (str, obj) {
+    return this.project.prefix('kik:' + str, obj);
+  }
+
+  targetFolder () {
+    if (!this.subfolder) return this.project.folder;
+    return path.join(this.project.folder, this.subfolder);
+  }
+
+  variables () {
+    var p = this.project.variables();
+    var s = this.prefix(this.name + ':', mix({}, [this.context, {
+      name: this.name,
+      folder: this.targetFolder()
+    }]));
+
+    return mix(p, [s]);
+  }
 }
 
 function formatVars (obj) {
@@ -97,3 +96,5 @@ function formatVars (obj) {
 
   return result;
 }
+
+module.exports = Starter;
